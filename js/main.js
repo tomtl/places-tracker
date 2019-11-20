bbf8797e9d3edc57aabf26e04979bde306a7f1e3
Tom Lee <tomtleee@gmail.com>
2019-11-20 07:58:25 -0500

    Add places layer to map

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 require([
     "esri/Map",
-    "esri/views/MapView"
-  ], function(Map, MapView) {
+    "esri/views/MapView",
+    "esri/layers/FeatureLayer"
+], function(Map, MapView, FeatureLayer) {
 
-  var map = new Map({
-    basemap: "topo-vector"
-  });
+    var map = new Map({
+        basemap: "topo-vector"
+    });
 
-  var view = new MapView({
-    container: "viewDiv",
-    map: map,
-    center: [-118.80500, 34.02700], // longitude, latitude
-    zoom: 13
-  });
+    var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [-71.055185, 42.3591112], // longitude, latitude
+        zoom: 13
+    });
+
+    // Places feature layer
+    var placesLayer = new FeatureLayer({
+        // url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+        url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/Places/FeatureServer/0"
+    });
+
+    map.add(placesLayer);
 });
